c3fcfa7c54cf65d962a12f0cf6871e161d07f7fb
mahakalpremier-max <mahakalpremier@gmail.com>
2026-06-14 16:48:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-if name == "main":
+if __name__ == "__main__":
     import asyncio
     asyncio.set_event_loop(asyncio.new_event_loop())
     app.run_polling()
